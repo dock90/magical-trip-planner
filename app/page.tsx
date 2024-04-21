127dff63d6a1c97cea2ab48a2bce076aed1c9737
EdwardGoomba <edward@dock90.io>
2024-04-21 11:55:28 -0600

    View all trips in trip dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
+'use client';
 import Link from 'next/link'
 import { Layout } from '@/components/layout'
+import { gql, useQuery } from '@apollo/client';
+
+// const statuses = {
+//   Paid: 'text-green-700 bg-green-50 ring-green-600/20',
+//   Withdraw: 'text-gray-600 bg-gray-50 ring-gray-500/10',
+//   Overdue: 'text-red-700 bg-red-50 ring-red-600/10',
+// }
+
+const GET_TRIPS = gql`
+  query GetTrips {
+    trips {
+      id
+      tripTitle
+      tripSummary
+    }
+  }
+`;
 
 export default function Home() {
+  const { data, loading, error } = useQuery(GET_TRIPS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const { trips } = data;
+
   return (
     <Layout>
       <div className="md:flex md:items-center md:justify-between">
@@ -27,6 +52,46 @@ export default function Home() {
           </Link>
         </div>
       </div>
+      <ul role="list" className="grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-3 xl:gap-x-8 mt-10">
+        {trips.map((trip) => {
+          const { id, tripTitle, tripSummary } = trip;
+
+          return (
+            <li key={id} className="overflow-hidden rounded-xl border border-gray-200 bg-white">
+              <div className="flex items-center gap-x-4 border-b border-gray-900/5 bg-gray-50 p-6">
+                {/* <img
+                  src={client.imageUrl}
+                  alt={client.name}
+                  className="h-12 w-12 flex-none rounded-lg bg-white object-cover ring-1 ring-gray-900/10"
+                /> */}
+                <div className="text-sm font-medium leading-6 text-gray-900">{tripTitle}</div>
+              </div>
+              {/* <dl className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
+                <div className="flex justify-between gap-x-4 py-3">
+                  <dt className="text-gray-500">Last invoice</dt>
+                  <dd className="text-gray-700">
+                    <time dateTime={client.lastInvoice.dateTime}>{client.lastInvoice.date}</time>
+                  </dd>
+                </div>
+                <div className="flex justify-between gap-x-4 py-3">
+                  <dt className="text-gray-500">Amount</dt>
+                  <dd className="flex items-start gap-x-2">
+                    <div className="font-medium text-gray-900">{client.lastInvoice.amount}</div>
+                    <div
+                      className={classNames(
+                        statuses[client.lastInvoice.status],
+                        'rounded-md py-1 px-2 text-xs font-medium ring-1 ring-inset'
+                      )}
+                    >
+                      {client.lastInvoice.status}
+                    </div>
+                  </dd>
+                </div>
+              </dl> */}
+            </li>
+          )
+        })}
+      </ul>
     </Layout>
   )
 }
